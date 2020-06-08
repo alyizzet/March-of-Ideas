ab1767e7ca566fb24cde83a4d83fb9c99a0acac7
Ali Izzet Metin <alyizzet@gmail.com>
2020-06-08 10:59:55 +0300

    Add files via upload

---
+++ hegelian_history/script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2 
+const holdTime = (totalTime / 5)
+
+breathAnimation();
+
+function breathAnimation() {
+    text.innerText = 'A Thesis is manifests!';
+    container.className = 'container grow';
+
+
+    setTimeout(()=> {
+        text.innerText = 'Antithesis grows!';
+
+        setTimeout(()=> {
+            text.innerText = 'Synthesis!';
+            container.className = 'container shrink';
+        }, holdTime);
+    }, breatheTime)
+    
+
+}
+
+setInterval((breathAnimation,totalTime));
